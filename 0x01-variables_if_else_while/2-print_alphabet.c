9ed71e9defe4a80244c032e571cb6db4bdbfcb52
Abdulfattahmjb <consortiummjb@gmail.com>
2023-05-20 07:12:36 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* main-This is the program entry point*/
+/* Return: Always return 0*/
+
+int main(void)
+
+	{
+	int i;
+
+	for (i = 0; i < 26; i++)
+
+	{
+		putchar('a' + i);
+	}
+	putchar('\n');
+
+	return (0);
+	}
+
