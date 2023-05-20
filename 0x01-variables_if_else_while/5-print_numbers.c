49a43fbe4771b8c8bab413281e2e9e498aeffa50
Abdulfattahmjb <consortiummjb@gmail.com>
2023-05-20 07:44:42 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*main-entry point*/
+/*Return: Always 0*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar('0' + i);
+	}
+	putchar('\n');
+
+	return (0);
+}
